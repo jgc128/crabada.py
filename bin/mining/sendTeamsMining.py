a437a623cc3768ad472ebcc318858717bfb295e5
Alexey Romanov <jgc128@outlook.com>
2022-04-30 20:28:33 +0000

    No mine file

--- bin/mining/sendTeamsMining.py
+++ bin/mining/sendTeamsMining.py
@@ -9,6 +9,7 @@ Usage:
 Author:
     @coccoinomane (Twitter)
 """
+from pathlib import Path
 
 from src.bot.mining.sendTeamsMining import sendTeamsMining
 from src.helpers.general import secondOrNone
@@ -17,6 +18,11 @@ from src.common.logger import logger
 from sys import argv, exit
 
 userAddress = secondOrNone(argv)
+
+no_mine_path = Path(__file__).parent.parent.joinpath(f'.no_mine_{userAddress}')
+if no_mine_path.exists():
+    logger.error(f"No mine: {userAddress}")
+    exit(1)
 
 if not userAddress:
     logger.error("Specify a user address")
